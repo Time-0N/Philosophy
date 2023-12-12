f12314c3355f01299b133cd46ab8d71134da11ad
Time_ON <haas.timeon@gmail.com>
2023-12-12 14:50:14 +0100

    Initial commit

---
+++ src/main/java/com/example/philosophy/PhilosophyApplication.java
@@ -0,0 +1,13 @@
+package com.example.philosophy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhilosophyApplication.class, args);
+    }
+
+}
